f6c65b2915b7cc9ab2deb17bb0a699176b206445
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2023-09-24 14:31:44 +0200

    Do not export ...TRIAL.SPACE symbols in grid3.lisp
    The package is defined with DEFPACKAGE* in package.lisp which already
    takes care of exporting all external ORG.SHIRAKUMO.FRAF.TRIAL.SPACE
    symbols.

--- grid3.lisp
+++ grid3.lisp
@@ -1,8 +1,5 @@
 (in-package #:org.shirakumo.fraf.trial.space.grid3)
 
-(do-external-symbols (s '#:org.shirakumo.fraf.trial.space)
-  (export s *package*))
-
 (declaim (inline clamp))
 (defun clamp (min x max)
   (max min (min x max)))
