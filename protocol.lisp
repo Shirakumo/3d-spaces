fc84a59bf062ff4fbc92e4e12bf271e6d381dd6f
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2023-10-12 12:18:49 +0200

    Add fine ray intersection test to default CALL-WITH-INTERSECTING method
    Before this change, the default CALL-WITH-INTERSECTING method just
    tested for overlap with a specific region based on the ray origin and
    direction.
    
    With this change, a two-step approach is used instead:
    
    1. Test overlap with a region that is practically infinite in one
       direction along each axis according to the ray direction.
    
    2. Filter the overlapping candidate objects using
       RAY-INTERSECTS-BOX-P.

--- protocol.lisp
+++ protocol.lisp
@@ -16,7 +16,7 @@
 (defgeneric call-with-candidates (function container region))
 (defgeneric call-with-overlapping (function container region))
 (defgeneric call-with-contained (function container region))
-(defgeneric call-with-intersecting (funciton container ray-origin ray-direction))
+(defgeneric call-with-intersecting (function container ray-origin ray-direction))
 
 (defgeneric serialize (container file object->id))
 (defgeneric deserialize (container file id->object))
@@ -319,6 +319,62 @@
          (and (<= (abs (- (vx2 ol) (vx2 rl))) (- (* 0.5f0 (vx3 s)) (vx2 ob)))
               (<= (abs (- (vy2 ol) (vy2 rl))) (- (* 0.5f0 (vy3 s)) (vx2 ob)))))))))
 
+(declaim (inline ray-intersects-box-p))
+(defun ray-intersects-box-p (ray-origin ray-direction box-min box-max
+                             &key (eps 1f-10))
+  (macrolet ((handle-zero-direction-axis (accessor)
+               `(when (< (abs (,accessor ray-direction)) eps)
+                  ;; The ray is parallel to the axis in
+                  ;; question. Check that the component of the ray
+                  ;; origin is within the box with respect to the axis
+                  ;; in question, return early if not.
+                  (unless (<= (,accessor box-min)
+                              (,accessor ray-origin)
+                              (,accessor box-max))
+                    (return-from ray-intersects-box-p nil))
+                  ;; Otherwise, prevent division by zero and mask out
+                  ;; the axis from the VMIN/VMAX computations below.
+                  (setf (,accessor direction) 1
+                        (,accessor min-mask) most-negative-single-float
+                        (,accessor max-mask) most-positive-single-float)))
+             (find-intersection (dimensions)
+               `(let ((direction (vec ray-direction))
+                      (min-mask ,(ecase dimensions
+                                   (2 `(vec #1=most-positive-single-float #1#))
+                                   (3 `(vec #2=most-positive-single-float #2# #2#))))
+                      (max-mask ,(ecase dimensions
+                                   (2 `(vec #3=most-negative-single-float #3#))
+                                   (3 `(vec #4=most-negative-single-float #4# #4#)))))
+                  ,@(ecase dimensions
+                      (2
+                       `((handle-zero-direction-axis vx)
+                         (handle-zero-direction-axis vy)))
+                      (3
+                       `((handle-zero-direction-axis vx)
+                         (handle-zero-direction-axis vy)
+                         (handle-zero-direction-axis vz))))
+                  (let* ((x1 (v- box-min ray-origin))
+                         (x2 (v- box-max ray-origin))
+                         (t1 (v/ x1 direction))
+                         (t2 (v/ x2 direction))
+                         (vmin (vmin t1 t2 min-mask))
+                         (vmax (vmax t1 t2 max-mask))
+                         (min ,(ecase dimensions
+                                 (2 `(max (vx vmin) (vy vmin)))
+                                 (3 `(max (vx vmin) (vy vmin) (vz vmin)))))
+                         (max ,(ecase dimensions
+                                 (2 `(min (vx vmax) (vy vmax)))
+                                 (3 `(min (vx vmax) (vy vmax) (vz vmax))))))
+                    (when (or (<= 0 min max) (<= min 0 max))
+                      (values min max))))))
+    (etypecase ray-origin
+      (vec3
+       (locally (declare (type vec3 ray-direction box-min box-max))
+         (find-intersection 3)))
+      (vec2
+       (locally (declare (type vec2 ray-direction box-min box-max))
+         (find-intersection 2))))))
+
 (defmethod serialize ((container container) file (object->id symbol))
   (serialize container file (fdefinition object->id)))
 
@@ -383,17 +439,59 @@
     (ensure-region thing region)
     (call-with-contained function container region)))
 
+;;; Use this as the size along one dimension for the box that is
+;;; constructed from the ray passed to CALL-WITH-INTERSECTING when
+;;; looking for candidate objects. The value should be large enough so
+;;; that the resulting box contains all objects that can occur in
+;;; practice. The value should be small enough to avoid large rounding
+;;; errors due to different sizes of the floating point numbers.
+(defconstant +RAY-INTERSECTION-SCAN-EXTENT+ 1f5)
+
+(declaim (inline ray-scan-region))
+(defun ray-scan-region (ray-origin ray-direction)
+  (macrolet ((extend (value zero-value)
+               `(let ((value ,value))
+                  (if (zerop value)
+                      ,zero-value
+                      (* +RAY-INTERSECTION-SCAN-EXTENT+ (float-sign value 1.0f0)))))
+             (compute-region (dimension)
+               `(let* ((ray-start ray-origin)
+                       (ray-end ,(ecase dimension
+                                   (2 `(vec2 (extend (vx ray-direction) (vx ray-start))
+                                             (extend (vy ray-direction) (vy ray-start))))
+                                   (3 `(vec3 (extend (vx ray-direction) (vx ray-start))
+                                             (extend (vy ray-direction) (vy ray-start))
+                                             (extend (vz ray-direction) (vz ray-start))))))
+                       (ray-min (vmin ray-start ray-end))
+                       (ray-max (vmax ray-start ray-end))
+                       (ray-size (v- ray-max ray-min)))
+                  (region (vx ray-min) (vy ray-min) ,(ecase dimension
+                                                       (2 0.0f0)
+                                                       (3 `(vz ray-min)))
+                          (vx ray-size) (vy ray-size) ,(ecase dimension
+                                                         (2 0.0f0)
+                                                         (3 `(vz ray-size)))))))
+    (etypecase ray-origin
+      (vec2 (compute-region 2))
+      (vec3 (compute-region 3)))))
+
 (defmethod call-with-intersecting (function (container container) ray-origin ray-direction)
-  (let ((region (etypecase ray-origin
-                  (vec2 (region (vx ray-origin) (vy ray-origin) 0.0
-                                (+ (vx ray-origin) (vx ray-direction))
-                                (+ (vy ray-origin) (vy ray-direction))
-                                0.0))
-                  (vec3 (region (vx ray-origin) (vy ray-origin) (vz ray-origin)
-                                (+ (vx ray-origin) (vx ray-direction))
-                                (+ (vy ray-origin) (vy ray-direction))
-                                (+ (vz ray-origin) (vz ray-direction)))))))
-    (call-with-overlapping function container region)))
+  ;; Since the ray is infinite, construct a region that is unbounded
+  ;; in one direction along each axis according to the components of
+  ;; RAY-DIRECTION.
+  (let ((function (ensure-function function))
+        (region (ray-scan-region ray-origin ray-direction)))
+    ;; Since REGION generally overlaps too many objects, filter the
+    ;; considered objects using a fine ray intersection test.
+    (flet ((consider (object)
+             (let* ((location (location object))
+                    (size/2 (bsize object))
+                    (bb-min (v- location size/2))
+                    (bb-max (v+ location size/2)))
+               (when (ray-intersects-box-p ray-origin ray-direction bb-min bb-max)
+                 (funcall function object)))))
+      (declare (dynamic-extent #'consider))
+      (call-with-overlapping #'consider container region))))
 
 (defun describe-tree (node children-fun stream)
   (fresh-line stream)
