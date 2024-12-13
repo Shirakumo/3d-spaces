5fbeafb96abe2f55403c4a917a065ca9e2a24f65
Yukari Hafner <shinmera@tymoon.eu>
2024-12-13 23:06:38 +0100

    Regen docs

--- staple.ext.lisp
+++ staple.ext.lisp
@@ -2,7 +2,8 @@
   '(#:org.shirakumo.fraf.trial.space
     #:org.shirakumo.fraf.trial.space.bvh2
     #:org.shirakumo.fraf.trial.space.quadtree
-    #:org.shirakumo.fraf.trial.space.grid3))
+    #:org.shirakumo.fraf.trial.space.grid3
+    #:org.shirakumo.fraf.trial.space.kd-tree))
 
 (defmethod staple:subsystems ((system (eql (asdf:find-system :3d-spaces))))
   ())
